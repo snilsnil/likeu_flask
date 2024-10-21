f738daa9e6f98b34552fd483aab88bfc737dc2d7
snilsnil <heo6601@naver.com>
2024-10-21 21:22:23 +0900

    modified start shooting frame

--- app.py
+++ app.py
@@ -31,53 +31,62 @@ def calculate_angle(point1, point2, point3):
 def process_video(video_path, csv_filename):
     cap = cv2.VideoCapture(video_path)
     frame_number = 1
+    
+    # 허용 오차 설정
+    height_tolerance = 0.01
+    
+    # 슛 던지는 시작 프레임
+    start_frame=0
 
     with open(csv_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         # Updated CSV header to include keypoints
         header = ['frame_number', 'keypoints', 'elbow_angle', 'knee_angle']
         writer.writerow(header)
-
+        
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
 
             # 프레임에서 포즈 추출
-            results = model(frame)
-            keypoints = results[0].keypoints.xyn.cpu().numpy()
-            boxes = results[0].boxes.xyxy.cpu().numpy()
+            results = model.track(source=frame, persist=True)  # persist=True로 ID 유지
+            
+            for result in results:
+                boxes = result.boxes  # 각 추적된 객체의 바운딩 박스 정보
+                keypoints = result.keypoints  # 각 객체의 keypoints 정보
+                
+                for i, box in enumerate(boxes):
+                    track_id = int(box.id) if box.id is not None else -1  # 추적 ID
+                    
+                    if track_id == 1:  # ID가 1인 객체 필터링
+                        keypoint=keypoints.xyn.cpu().numpy()  # 키포인트 좌표 
+                        
+                        # 관절 높이 가져오기
+                        left_shoulder_y = keypoint[0][6][1]  # 왼쪽 어깨의 y 좌표
+                        left_elbow_y = keypoint[0][8][1]     # 왼쪽 팔꿈치의 y 좌표
+                        
+                        # 팔꿈치가 어깨 높이와 동일할 때
+                        if abs(left_elbow_y - left_shoulder_y) <= height_tolerance and start_frame==0:
+                            start_frame=frame_number
+                        
+                        if start_frame <= frame_number and start_frame != 0:
+                            
+                            # Calculating the angles
+                            left_shoulder = keypoint[0][5][:2]
+                            left_elbow = keypoint[0][7][:2]
+                            left_wrist = keypoint[0][9][:2]
+                            elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
 
-            max_area = 0
-            selected_person = None
+                            left_hip = keypoint[0][11][:2]
+                            left_knee = keypoint[0][13][:2]
+                            left_ankle = keypoint[0][15][:2]
+                            knee_angle = calculate_angle(left_hip, left_knee, left_ankle)
+                            
+                            row = [keypoint[0][i][0] for i in range(1, 17)]  # x 좌표 저장
+                            writer.writerow([frame_number, row, elbow_angle, knee_angle])
 
-            for person, box in zip(keypoints, boxes):
-                x1, y1, x2, y2 = box
-                area = (x2 - x1) * (y2 - y1)
-
-                if area > max_area:
-                    max_area = area
-                    selected_person = person
-
-            if selected_person is not None and 94 <= frame_number <= 132:
-                # Calculating the angles
-                left_shoulder = selected_person[5][:2]
-                left_elbow = selected_person[7][:2]
-                left_wrist = selected_person[9][:2]
-                elbow_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
-
-                left_hip = selected_person[11][:2]
-                left_knee = selected_person[13][:2]
-                left_ankle = selected_person[15][:2]
-                knee_angle = calculate_angle(left_hip, left_knee, left_ankle)
-
-                # Saving the keypoints as a list of tuples
-                keypoints_list = [(kp[0], kp[1]) for kp in selected_person]
-
-                # Writing the data into the CSV file
-                writer.writerow([frame_number, keypoints_list, elbow_angle, knee_angle])
-
-            frame_number += 1
+                        frame_number += 1
 
     cap.release()
 
